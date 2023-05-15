8f9e148b0309108098f0b44a238d776cad945877
Luis Anaya <brothersailorwebdev@gmail.com>
2023-05-15 22:44:43 +0200

    final

--- components/pages/project/ProjectPreview.tsx
+++ components/pages/project/ProjectPreview.tsx
@@ -1,21 +1,29 @@
-'use client';
+// // 'use client';
 
-import { usePreview } from 'lib/sanity.preview';
-import { projectBySlugQuery } from 'lib/sanity.queries';
-import type { ProjectPayload } from '@/types/sanity';
+// // import { usePreview } from 'lib/sanity.preview';
+// // import { projectBySlugQuery } from 'lib/sanity.queries';
+// // import type { ProjectPayload } from '@/types/sanity';
 
-import { ProjectPage } from './ProjectPage';
+// // import { ProjectPage } from './ProjectPage';
 
-export function ProjectPreview({
-  token,
-  slug,
-}: {
-  token: null | string;
-  slug: string;
-}) {
-  const project: ProjectPayload = usePreview(token, projectBySlugQuery, {
-    slug: slug,
-  });
+// // export function ProjectPreview({
+// //   token,
+// //   slug,
+// // }: {
+// //   token: null | string;
+// //   slug: string;
+// // }) {
+// //   const project: ProjectPayload = usePreview(token, projectBySlugQuery, {
+// //     slug: slug,
+// //   });
 
-  return <ProjectPage data={project} />;
-}
+// //   return <ProjectPage data={project} />;
+// // }
+
+import React from 'react';
+
+const ProjectPreview = () => {
+  return <div>ProjectPreview</div>;
+};
+
+export default ProjectPreview;
